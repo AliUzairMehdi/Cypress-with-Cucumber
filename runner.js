fbde8ae6b27fb59b191255c868a3fea51b344506
aliuzairmehdi123 <i170538@nu.edu.pk>
2024-02-28 01:13:20 +0500

    Runner JS File

---
+++ runner.js
@@ -0,0 +1,18 @@
+const { exec } = require("child_process");
+
+const flowName = process.argv[2];
+const browser = process.argv[3];
+const runMode = `--${process.argv[4]}`;
+const env = "";
+
+exec(
+  `npx cypress run --env flowName=${flowName}Flow --browser ${browser} ${runMode} ${env}`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  }
+);
