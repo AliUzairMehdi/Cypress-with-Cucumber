f1eed32f16dc76140e7a73d9e7f5306f894aff87
aliuzairmehdi123 <i170538@nu.edu.pk>
2024-02-28 20:30:28 +0500

    Home Page Selectors

---
+++ cypress/e2e/main/UI/homePage/selectors/homePage.selectors.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getHomePageLoginButton: `#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a`,
+  getL1TabProductButton: `.//li[contains(.,"Products")]`,
+  getHomePageSlider: `.slide`,
+};
