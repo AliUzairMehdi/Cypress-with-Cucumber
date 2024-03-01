2202f04456cc5281deaa9e931b1b36676af84089
aliuzairmehdi123 <i170538@nu.edu.pk>
2024-03-01 14:35:12 +0500

    changed Directory

--- cypress/e2e/main/UI/homePage/tasks/homePage.tasks.js
+++
@@ -1,9 +0,0 @@
-const homePageActions = require("../actions/homePage.actions");
-
-function userNavigatesToAuthenticationPage() {
-  homePageActions.clickLoginSignUpButton();
-}
-
-module.exports = {
-  userNavigatesToAuthenticationPage,
-};
