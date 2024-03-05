40a7721a2041cc5311bb6c840e5c3271e99ef55f
aliuzairmehdi123 <i170538@nu.edu.pk>
2024-03-06 03:00:24 +0500

    Browser Assertions Utils

--- cypress/main/utils/browserAssertions.utils.js
+++ cypress/main/utils/browserAssertions.utils.js
@@ -4,11 +4,23 @@ function verifyElementsAreEqual(actual, expected, message) {
   expect(actual).to.equal(expected, message);
 }
 
-function verifyElementIsExisting(status, errorMessage) {
-  expect(status).to.equal(true, errorMessage);
+function verifyPageIsOpened(expectedUrl, errorMessage, isLast) {
+  cy.getUrl().then((url) => {
+    assert.softAssert(url, expectedUrl, errorMessage);
+    if (isLast) assert.softAssertAll();
+  });
+}
+
+function verifyElementIsExisting(selector, errorMessage, isLast) {
+  cy.getLength(selector).then((length) => {
+    let status = length > 0;
+    assert.softTrue(status, errorMessage);
+    if (isLast) assert.softAssertAll();
+  });
 }
 
 module.exports = {
   verifyElementIsExisting,
   verifyElementsAreEqual,
+  verifyPageIsOpened,
 };
